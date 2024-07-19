f0115e5598103c9c2291bbcad86227453a455b71
Mitka92 <mashkovskiy1992@gmail.com>
2024-07-19 18:59:09 +0300

    add Profile styles
    add Profile styles
    add modern-normalize

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,26 @@
+import css from "./Profile.module.css";
+
 function Profile({ name, tag, location, image, stats }) {
   return (
-    <div>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css["profile-container"]}>
+      <div className={css["profile-info"]}>
+        <img className={css["profile-img"]} src={image} alt="User avatar" />
+        <p className={css["profile-info-name"]}>{name}</p>
+        <p className={css["profile-info-text"]}>@{tag}</p>
+        <p className={css["profile-info-text"]}>{location}</p>
       </div>
-      <ul>
-        <li>
+      <ul className={css["profile-stats-list"]}>
+        <li className={css["profile-stats-item"]}>
           <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={css["profile-stats-count"]}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css["profile-stats-item"]}>
           <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={css["profile-stats-count"]}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css["profile-stats-item"]}>
           <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={css["profile-stats-count"]}>{stats.likes}</span>
         </li>
       </ul>
     </div>
