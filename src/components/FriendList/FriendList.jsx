8b6068152463eceb963d005d8c6984641bfe7845
Mitka92 <mashkovskiy1992@gmail.com>
2024-07-19 21:49:39 +0300

    FriendList edit

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,8 +1,8 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
-
+import css from "./FriendList.module.css";
 function FriendList({ friends }) {
   return (
-    <ul>
+    <ul className={css["friends-list"]}>
       {friends.map((friend) => {
         return (
           <li key={friend.id}>
